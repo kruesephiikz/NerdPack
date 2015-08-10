8eddbd7dd0cbd94d145a7d0f5719106124473f70
Jaime Marques <mrthesoulz@gmail.com>
2015-08-10 23:54:37 +0100

    Ground Work for Walock Destro

--- rotations/warlock/destro.lua
+++ rotations/warlock/destro.lua
@@ -2,50 +2,62 @@ local exeOnLoad = function()
 	NeP.Splash()
 end
 
-local inCombat = {
-	
+local _General = {
 	-- Keybinds
-		{ "Rain of Fire", "modifier.alt", "ground" },
+	{ "Rain of Fire", "modifier.alt", "ground" },
 
 	--Buffs
-		{ "Dark Intent", "!player.buff" },
-  		{ "Curse of the Elements", "!target.debuff" },
+	{ "Dark Intent", "!player.buff" },
+	{ "Curse of the Elements", "!target.debuff" },
+}
 
-	-- Cooldowns
-  		{"Dark Soul: Instability", {"modifier.shift", "modifier.cooldowns"}},
-  		{"Summon Terrorguard", {"modifier.control", "modifier.cooldowns"}},
-  		{"Summon Doomguard", {"modifier.control", "modifier.cooldowns"}},
+local _Cooldowns = {
+	{"Dark Soul: Instability", "modifier.shift"},
+  	{"Summon Terrorguard", "modifier.control"},
+  	{"Summon Doomguard", "modifier.control"},
+}
 
-	-- Moving
-		{ "Incinerate", {"player.spell(Kil'jaeden's Cunning).exists", "player.moving"} },
-  		{ "Fel Flame", {"!player.spell(Kil'jaeden's Cunning).exists", "player.moving"} }, 	
-		
+local _Moving = {
+	{ "Incinerate", {
+		"player.spell(Kil'jaeden's Cunning).exists", 
+		"player.moving"
+	}, "target" },
+  	{ "Fel Flame", {
+		"!player.spell(Kil'jaeden's Cunning).exists", 
+		"player.moving"
+	}, "target" }, 
+}
+
+local inCombat = {
 	-- AoE
-		{"Fire and Brimstone", (function() return NeP.Lib.SAoE(3, 40) end), "target"},
+	{"Fire and Brimstone", (function() return NeP.Lib.SAoE(3, 40) end), "target"},
 
   	-- Rotation
-	  	{"Shadowburn", "target.health <=20", "target"},
-	  	{"Immolate", "target.debuff(Immolate).duration <= 4", "target"},
-	  	{"Conflagrate", "player.spell(Conflagrate).charges >= 2", "target"},
-	  	{"Chaos Bolt", {"!lastcast(Chaos Bolt)", "player.embers >= 35"}, "target"},
-		{"Chaos Bolt", "player.buff(Dark Soul: Instability)", "target"},
-		{"Chaos Bolt", "player.buff(Skull Banner)", "target"},
-	  	{"Conflagrate" },
-	  	{"Incinerate" },
-
-
+	{"Shadowburn", "target.health <=20", "target"},
+	{"Immolate", "target.debuff(Immolate).duration <= 4", "target"},
+	{"Conflagrate", "player.spell(Conflagrate).charges >= 2", "target"},
+	{"Chaos Bolt", {
+		"!lastcast(Chaos Bolt)", 
+		"player.embers >= 35"
+	}, "target"},
+	{"Chaos Bolt", "player.buff(Dark Soul: Instability)", "target"},
+	{"Chaos Bolt", "player.buff(Skull Banner)", "target"},
+	{"Conflagrate" },
+	{"Incinerate" },
 }
 
 local outCombat = {
 
-	--Buffs
-		{ "Dark Intent", "!player.buff" },
-  		{ "Curse of the Elements", "!target.debuff" },
-
-	-- Keybinds
-		{ "Rain of Fire", "modifier.alt", "ground" },
-
 }
 
 ProbablyEngine.rotation.register_custom(267, NeP.Core.GetCrInfo('Warlock - Destruction'),
-	inCombat, outCombat, exeOnLoad)
+	{ -- In-Combat
+		{_Moving, "player.moving"},
+		{{ -- Conditions
+			{_Cooldowns, "modifier.cooldowns"},
+			{inCombat}
+		}, "!player.moving" },
+	},{ -- Out-Combat
+		{_General},
+		{outCombat}
+	}, exeOnLoad)
