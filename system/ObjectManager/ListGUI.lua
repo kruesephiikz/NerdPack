f93a03ab8d03b13216a8d397248f9c3ecc045858
Jaime Marques <mrthesoulz@gmail.com>
2015-08-15 09:25:23 +0100

    Fix OM

--- system/ObjectManager/ListGUI.lua
+++ system/ObjectManager/ListGUI.lua
@@ -6,17 +6,24 @@ local _Displaying = 'Friendly List'
 local OPTIONS_WIDTH = 510
 local OPTIONS_HEIGHT = 210
 
---parent NeP.Addon.Interface.OMGUI 
-NeP.Addon.Interface.OMGUI = CreateFrame("Frame", "ObjectManager", UIParent) 
-NeP.Addon.Interface.OMGUI.Open = false
-NeP.Addon.Interface.OMGUI:SetSize(OPTIONS_WIDTH, OPTIONS_HEIGHT) 
-NeP.Addon.Interface.OMGUI:SetPoint("CENTER") 
-NeP.Addon.Interface.OMGUI.texture = NeP.Addon.Interface.OMGUI:CreateTexture() 
-NeP.Addon.Interface.OMGUI.texture:SetAllPoints() 
-NeP.Addon.Interface.OMGUI.texture:SetTexture(0,0,0,0.7)
+--NeP.Addon.Interface.OMGUI
 
-local title = CreateFrame("Frame", nil, NeP.Addon.Interface.OMGUI)
-title:SetPoint("TOP", NeP.Addon.Interface.OMGUI)
+--parent mainFrame 
+mainFrame = CreateFrame("Frame", "ObjectManager", UIParent) 
+mainFrame:SetSize(OPTIONS_WIDTH, OPTIONS_HEIGHT) 
+mainFrame:SetPoint("CENTER") 
+mainFrame.texture = mainFrame:CreateTexture() 
+mainFrame.texture:SetAllPoints() 
+mainFrame.texture:SetTexture(0,0,0,0.7)
+mainFrame:SetMovable(true)
+mainFrame:RegisterForDrag("LeftButton")
+mainFrame:EnableMouse(true)
+mainFrame:SetScript("OnDragStart", mainFrame.StartMoving)
+mainFrame:SetScript("OnDragStop", mainFrame.StopMovingOrSizing)
+
+
+local title = CreateFrame("Frame", nil, mainFrame)
+title:SetPoint("TOP", mainFrame)
 title:SetWidth(OPTIONS_WIDTH)
 title:SetHeight(30)
 title.texture = title:CreateTexture() 
@@ -36,13 +43,13 @@ title.text3:SetPoint("RIGHT", -100, 0)
 title.text3:SetText('')
  
  -- Close Button
-local closeButton = CreateFrame("Button", nil, NeP.Addon.Interface.OMGUI)
+local closeButton = CreateFrame("Button", nil, mainFrame)
 closeButton:SetPoint("BOTTOMLEFT", 0, 0)
 closeButton:SetWidth(100)
 closeButton:SetHeight(25)
 closeButton:SetText("Close")
 closeButton:SetNormalFontObject("GameFontNormal")
-closeButton:SetScript("OnClick", function(self) NeP.Addon.Interface.OMGUI:Hide() end)
+closeButton:SetScript("OnClick", function(self) mainFrame:Hide() end)
 closeButton.closeButton1 = closeButton:CreateTexture()
 closeButton.closeButton1:SetTexture(0,0,0,1)
 closeButton.closeButton1:SetTexCoord(0, 0.625, 0, 0.6875)
@@ -60,7 +67,7 @@ closeButton:SetHighlightTexture(closeButton.closeButton2)
 closeButton:SetPushedTexture(closeButton.closeButton3)
 
 -- Enemy Button
-local enemyButton = CreateFrame("Button", nil, NeP.Addon.Interface.OMGUI)
+local enemyButton = CreateFrame("Button", nil, mainFrame)
 enemyButton:SetPoint("BOTTOMRIGHT", 0, 0)
 enemyButton:SetWidth(100)
 enemyButton:SetHeight(25)
@@ -84,7 +91,7 @@ enemyButton:SetHighlightTexture(enemyButton.closeButton2)
 enemyButton:SetPushedTexture(enemyButton.closeButton3)
 
 -- Friendly Button
-local friendlyButton = CreateFrame("Button", nil, NeP.Addon.Interface.OMGUI)
+local friendlyButton = CreateFrame("Button", nil, mainFrame)
 friendlyButton:SetPoint("BOTTOMRIGHT", -110, 0)
 friendlyButton:SetWidth(100)
 friendlyButton:SetHeight(25)
@@ -108,7 +115,7 @@ friendlyButton:SetHighlightTexture(friendlyButton.closeButton2)
 friendlyButton:SetPushedTexture(friendlyButton.closeButton3)
 
 -- Object Button
-local ObjectButton = CreateFrame("Button", nil, NeP.Addon.Interface.OMGUI)
+local ObjectButton = CreateFrame("Button", nil, mainFrame)
 ObjectButton:SetPoint("BOTTOMRIGHT", -220, 0)
 ObjectButton:SetWidth(100)
 ObjectButton:SetHeight(25)
@@ -131,52 +138,55 @@ ObjectButton:SetNormalTexture(ObjectButton.closeButton1)
 ObjectButton:SetHighlightTexture(ObjectButton.closeButton2)
 ObjectButton:SetPushedTexture(ObjectButton.closeButton3)
 
-local contentFrame = CreateFrame("Frame", nil, NeP.Addon.Interface.OMGUI) 
-contentFrame:SetSize(OPTIONS_WIDTH, OPTIONS_HEIGHT - 60)  
-contentFrame:SetPoint("TOP", 0, -30) 
+local scrollframe = CreateFrame("ScrollFrame", nil, mainFrame) 
+scrollframe:SetSize(OPTIONS_WIDTH, OPTIONS_HEIGHT - 60)  
+scrollframe:SetPoint("TOP", 0, -30) 
  
---scrollframe 
-local scrollframe = CreateFrame("ScrollFrame", nil, contentFrame) 
-scrollframe:SetPoint("TOPLEFT", 0, 0) 
-scrollframe:SetPoint("BOTTOMRIGHT", 0, 0) 
-local texture = scrollframe:CreateTexture() 
+--contentFrame 
+local contentFrame = CreateFrame("Frame") 
+contentFrame:SetPoint("TOPLEFT", 0, 0) 
+contentFrame:SetPoint("BOTTOMRIGHT", 0, 0) 
+contentFrame:SetSize(OPTIONS_WIDTH, 0)
+local texture = contentFrame:CreateTexture() 
 texture:SetAllPoints() 
 texture:SetTexture(0,0,0,0.3)
-NeP.Addon.Interface.OMGUI.scrollframe = scrollframe 
+mainFrame.contentFrame = contentFrame 
  
---scrollbar 
-local scrollbar = CreateFrame("Slider", nil, scrollframe, "UIPanelScrollBarTemplate") 
-scrollbar:SetPoint("TOPLEFT", NeP.Addon.Interface.OMGUI, "TOPRIGHT", 0, -16) 
-scrollbar:SetPoint("BOTTOMLEFT", NeP.Addon.Interface.OMGUI, "BOTTOMRIGHT", 0, 16) 
-scrollbar:SetMinMaxValues(1, 200) 
-scrollbar:SetValueStep(1) 
-scrollbar.scrollStep = 1
-scrollbar:SetValue(0) 
-scrollbar:SetWidth(16) 
-scrollbar:SetScript("OnValueChanged", 
-function (self, value) 
-	self:GetParent():SetVerticalScroll(value) 
-end) 
-local scrollbg = scrollbar:CreateTexture(nil, "BACKGROUND") 
-scrollbg:SetAllPoints(scrollbar) 
-scrollbg:SetTexture(0, 0, 0, 0.4) 
-NeP.Addon.Interface.OMGUI.scrollbar = scrollbar 
- 
---content NeP.Addon.Interface.OMGUI 
-local content = CreateFrame("Frame", nil, scrollframe) 
-content:SetSize(OPTIONS_WIDTH, OPTIONS_HEIGHT+500) 
-content.texture = texture
+-- Create the slider that will be used to scroll through the results
+local scrollbar = CreateFrame("Slider", "FPreviewScrollBar", mainFrame)
+if not scrollbar.bg then
+   scrollbar.bg = scrollbar:CreateTexture(nil, "BACKGROUND")
+   scrollbar.bg:SetAllPoints(true)
+   scrollbar.bg:SetTexture(0, 0, 0, 0.5)
+end
+if not scrollbar.thumb then
+   scrollbar.thumb = scrollbar:CreateTexture(nil, "OVERLAY")
+   scrollbar.thumb:SetTexture("Interface\\Buttons\\UI-ScrollBar-Knob")
+   scrollbar.thumb:SetSize(25, 25)
+   scrollbar:SetThumbTexture(scrollbar.thumb)
+end
+--local scrollMax = 10
+scrollbar:SetOrientation("VERTICAL");
+scrollbar:SetSize(16, OPTIONS_HEIGHT)
+scrollbar:SetPoint("TOPLEFT", mainFrame, "TOPRIGHT", 0, 0)
+scrollbar:SetMinMaxValues(0, 10)
+scrollbar:SetValue(0)
+scrollbar:SetScript("OnValueChanged", function(self)
+      scrollframe:SetVerticalScroll(self:GetValue())
+end)
+
+scrollframe:SetScrollChild(contentFrame)
 
 local function getText()
-    scrollframe.text = tremove(Texts)
-    if not scrollframe.text then
-        scrollframe.text = scrollframe.content:CreateFontString(nil, "OVERLAY", scrollframe.content)
-		scrollframe.text:SetFont("Fonts\\FRIZQT__.TTF", 11)
-        scrollframe.text:SetParent(scrollframe.content)
+    contentFrame.text = tremove(Texts)
+    if not contentFrame.text then
+        contentFrame.text = contentFrame:CreateFontString(nil, "OVERLAY", contentFrame)
+		contentFrame.text:SetFont("Fonts\\FRIZQT__.TTF", 11)
+        contentFrame.text:SetParent(contentFrame)
     end
-    scrollframe.text:Show()
-    table.insert(TextsUsed, scrollframe.text)
-    return scrollframe.text
+    contentFrame.text:Show()
+    table.insert(TextsUsed, contentFrame.text)
+    return contentFrame.text
 end
 
 local function recycleTexts()
@@ -186,32 +196,52 @@ local function recycleTexts()
     end
 end
 
-C_Timer.NewTicker(1, (function()
+local height = 0
 
-		recycleTexts()
+C_Timer.NewTicker(0.1, (function()
 		
-		local currentRow = 0
-		title.text2:SetText('|cffff0000Total: '..#_objectTable)
-		title.text3:SetText('|cffCCCCCC('.._Displaying..')')
+	recycleTexts()
+	
+	height = 0
+	local currentRow = 0
+	title.text2:SetText('|cffff0000Total: '..#_objectTable)
+	title.text3:SetText('|cffCCCCCC('.._Displaying..')')
 
 		if #_objectTable > 0 then
 			for i=1,#_objectTable do
-				--if currentRow >= 11 then break end
-				local _object = _objectTable[i]
-				local guid = UnitGUID(_object.key)
-				local _, _, _, _, _, _id, _ = strsplit("-", guid)
-				local Text = getText()
-				local name = _object.name
-				local objectID = (tonumber(_id) or 'UNKNOWN')
-				local distance = _object.distance
-				scrollframe.text:SetPoint("TOPLEFT", 0, 0+ (currentRow * -15) + -currentRow)
-				scrollframe.text:SetText('|cffff0000Name: |r'..name..'|cffCCCCCC( Distance: '..distance..' ID: '..objectID..' )')
-				currentRow = currentRow + 1
-			end
+			--if currentRow >= 11 then break end
+			local _object = _objectTable[i]
+			local guid = UnitGUID(_object.key)
+			local _, _, _, _, _, _id, _ = strsplit("-", guid)
+			local Text = getText()
+			local name = _object.name
+			local objectID = (tonumber(_id) or 'UNKNOWN')
+			local distance = _object.distance
+			contentFrame.text:SetPoint("TOPLEFT", 0, 0+ (currentRow * -15) + -currentRow)
+			contentFrame.text:SetText('|cffff0000Name: |r'..name..'|cffCCCCCC( Distance: '..distance..' ID: '..objectID..' )')
+			--contentFrame.text:SetScript("OnMouseDown", function(self) TargetUnit(_object.key) end)
+			currentRow = currentRow + 1
+			height = height + contentFrame.text:GetStringHeight() + 5
 		end
+	end
+	contentFrame:SetSize(OPTIONS_WIDTH, height)
+	scrollbar:SetMinMaxValues(0, height)
 
 end), nil)
 
-scrollframe.content = content 
-scrollframe:SetScrollChild(content)
-NeP.Addon.Interface.OMGUI:Hide()
+contentFrame:EnableMouseWheel(true)
+contentFrame:SetScript("OnMouseWheel", function(self, delta)
+      local current = scrollbar:GetValue()
+       
+      if IsShiftKeyDown() and (delta > 0) then
+         scrollbar:SetValue(0)
+      elseif IsShiftKeyDown() and (delta < 0) then
+         scrollbar:SetValue(height)
+      elseif (delta < 0) and (current < height) then
+         scrollbar:SetValue(current + 20)
+      elseif (delta > 0) and (current > 1) then
+         scrollbar:SetValue(current - 20)
+      end
+end)
+
+mainFrame:Show()
