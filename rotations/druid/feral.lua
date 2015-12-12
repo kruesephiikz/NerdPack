0cf7e5e051c6ca4f13d7a2ac774cd33a8f2877e3
Jaime Marques <mrthesoulz@gmail.com>
2015-12-12 22:58:20 +0000

    small clean

--- rotations/druid/feral.lua
+++ rotations/druid/feral.lua
@@ -21,55 +21,22 @@ NeP.Interface.classGUIs[103] = {
 		      	text = "Form", 
 		      	key = "Form", 
 		      	list = {
-			        {
-			          text = "Cat",
-			          key = "2"
-			        },
-			        {
-			          text = "Bear",
-			          key = "1"
-			        },
-			        {
-			          text = "Travel",
-			          key = "3"
-			        },
-			        {
-			          text = "Normal",
-			          key = "0"
-			        },
-			        {
-			          text = "MANUAL",
-			          key = "MANUAL"
-			        }
+					{text = "Cat",key = "2"},
+					{text = "Bear",key = "1"},
+					{text = "Travel",key = "3"},
+					{text = "Normal",key = "0"},
+					{text = "MANUAL",key = "MANUAL"}
 			    }, 
 		    	default = "2", 
 		    	desc = "Select What form to use while in of combat" 
 		    },
-			{ 
-		      	type = "dropdown",
-		      	text = "Form OOC", 
-		      	key = "FormOCC", 
+			{ type = "dropdown", text = "Form OOC", key = "FormOCC", 
 		      	list = {
-			        {
-			          text = "Cat",
-			          key = "2"
-			        },
-			        {
-			          text = "Bear",
-			          key = "1"
-			        },
-			        {
-			          text = "Travel",
-			          key = "3"
-			        },
-			        {
-			          text = "Normal",
-			          key = "0"
-			        },
-			        {
-			          text = "MANUAL",
-			          key = "MANUAL"
-			        }
+					{text = "Cat",key = "2"},
+					{text = "Bear",key = "1"},
+					{text = "Travel",key = "3"},
+					{text = "Normal",key = "0"},
+					{text = "MANUAL",key = "MANUAL"}
 			    }, 
 		    	default = "0", 
 		    	desc = "Select What form to use while out of combat" 
@@ -82,19 +49,14 @@ NeP.Interface.classGUIs[103] = {
 		-- Player
 		{ type = 'rule' },
 		{ type = 'header', text = "Player settings:", align = "center"},
-
 			-- Tiger's Fury
 			{ type = "spinner", text = "Tigers Fury", key = "TigersFury", default = 35},
-
 			-- Renewal
 			{ type = "spinner", text = "Renewal", key = "Renewal", default = 30},
-
 			-- Cenarion Ward
 			{ type = "spinner", text = "Cenarion Ward", key = "CenarionWard", default = 75},
-
 			-- Survival Instincts
 			{ type = "spinner", text = "Survival Instincts", key = "SurvivalInstincts", default = 75},
-
 			-- Healing Touch
 			{ type = "spinner", text = "Healing Touch", key = "HealingTouch", default = 70, Desc=
 			"When player as buff (Predatory Swiftness)."},		
